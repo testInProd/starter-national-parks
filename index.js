b589259e30cd0aa39f5b89102b5db558a4d220e5
Luis Orozco <orozcolui@gmail.com>
2024-08-05 22:19:56 -0500

    adding class material

--- index.js
+++ index.js
@@ -35,4 +35,65 @@ header.appendChild(newElement);
 
 const main = document.querySelector("main");
 main.removeChild(main.querySelector(".park-display"));
-main.removeChild(main.querySelector(".park-display"));
+
+// Select all the buttons for all the parks
+const allBtns = document.querySelectorAll(".rate-button");
+
+// Iterate through the list of buttons and add an event handler to each
+allBtns.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    event.target.parentNode.style.backgroundColor = "#c8e6c9";
+  });
+});
+
+/// Function for sorting by name
+const sortByName = (parkA, parkB) => {
+  const parkAName = parkA.querySelector("h2").innerText;
+  const parkBName = parkB.querySelector("h2").innerText;
+  if (parkAName < parkBName) {
+    return -1;
+  } else if (parkAName > parkBName) {
+    return 1;
+  } else {
+    return 0;
+  }
+};
+
+// Function for handling the `nameSorter` click
+const nameSorterClickHandler = (event) => {
+  event.preventDefault();
+
+  // 1.  Get the main element
+  const main = document.querySelector("main");
+
+  // 2. Get the list of parks
+  const parksList = main.querySelectorAll(".park-display");
+
+  // 3. Empty the main
+  main.innerHTML = "";
+
+  // 4. Create an array
+  const parksArray = Array.from(parksList);
+
+  // 5. Sort the array
+  parksArray.sort(sortByName);
+
+  // 6. Insert each park into the DOM
+  parksArray.forEach((park) => {
+    main.appendChild(park);
+  });
+};
+
+// Select the `nameSorter` link
+const nameSorter = document.querySelector("#name-sorter");
+
+// Add an event listener
+nameSorter.addEventListener("click", nameSorterClickHandler);
+
+console.log("Before!");
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  console.log("Loaded!");
+});
+
+console.log("After!");
---
+++ temp.js
@@ -0,0 +1,6 @@
+function timerGame(name) {
+    console.log('Hi, ' + name);
+  }
+  
+  // write a setTimeout function so that it calls the timerGame function after 1 second and prints 'Hi, Thundercat'.
+  setTimeout(timerGame, 1000, "Thundercat");
